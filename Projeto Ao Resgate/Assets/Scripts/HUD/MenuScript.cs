ef4c94dba1e3573967d65a611b515f99fc231907
Pedro <pedrommarangon@gmail.com>
2019-06-11 16:30:36 -0300

    atualizado
    Organização das pastas;
    Arte provisória de power-ups;
    MÚSICA Q LIGA E DESLIGA;

---
+++ Projeto Ao Resgate/Assets/Scripts/HUD/MenuScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using NaughtyAttributes;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MenuScript : MonoBehaviour {
+
+	public AudioMixer mixer;
+
+	[BoxGroup("HUD Movement")][InfoBox("Put the X/Y/Z values in the order that the screens are in the Scene View")] public Vector3 xValues;
+	[BoxGroup("HUD Movement")]public float MoveDuration = .5f;
+	[BoxGroup("HUD Movement")]public RectTransform HUD;
+
+	[BoxGroup("SFX")]public GameObject sfxOn;
+	[BoxGroup("SFX")]public GameObject sfxOff;
+
+	[BoxGroup("MUSIC")]public GameObject mscOn;
+	[BoxGroup("MUSIC")]public GameObject mscOff;
+
+
+
+	// Use this for initialization
+	void Start () {
+		GoToMain();
+	}
+
+	public void GoToSettings(){
+		HUD.DOMoveX(xValues.x,MoveDuration);
+	}
+	
+	public void GoToMain(){
+		HUD.DOMoveX(xValues.y,MoveDuration);
+	}
+		
+	public void GoToShop(){
+		HUD.DOMoveX(xValues.z,MoveDuration);
+	}
+	
+	public void PlayGame(){
+		SceneManager.LoadScene("test");
+	}
+
+	public void SetMusicVolume(bool state){
+		mscOn.SetActive(state);
+		mscOff.SetActive(!state);
+		mixer.SetFloat("musicVolume",(state)?0:-80);
+	}
+
+	public void SetSFXVolume(bool state){
+		sfxOn.SetActive(state);
+		sfxOff.SetActive(!state);
+		mixer.SetFloat("sfxVolume",(state)?0:-80);
+	}
+
+}
